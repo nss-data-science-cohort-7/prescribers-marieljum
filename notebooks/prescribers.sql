0e60d39f34892f1c85e81e2ce6265998cf740260
marieljum <mjumawanm@gmail.com>
2024-02-08 22:08:28 -0600

    Working on 6

--- notebooks/prescribers.sql
+++ notebooks/prescribers.sql
@@ -67,39 +67,122 @@ WHERE total_claims IS NULL;
 
 -- 3a. Which drug (generic_name) had the highest total drug cost?
 
+SELECT generic_name, SUM(total_drug_cost) AS total_cost
+FROM prescription
+LEFT JOIN drug
+USING(drug_name)
+GROUP BY generic_name
+ORDER BY total_cost DESC;
 
+-- Insulin Glargine had the highest total drug cost at $104264066.35.
 
 -- 3b. Which drug (generic_name) has the hightest total cost per day? 
 -- Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.
+
+SELECT generic_name, ROUND(SUM(total_drug_cost) / SUM(total_day_supply), 2) as cost_per_day
+FROM prescription
+LEFT JOIN drug
+USING(drug_name)
+GROUP BY generic_name
+ORDER BY cost_per_day DESC;
+
+-- C1 Esterase Inhibitor has the highest cost per day at $3495.22.
 
 -- 4a. For each drug in the drug table, return the drug name and then a column named 'drug_type' 
 -- which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' 
 -- for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.
 
+SELECT 
+	drug_name, 
+	CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
+		WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+		ELSE 'neither'
+		END AS drug_type
+FROM drug;
+
+
 -- 4b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. 
 -- Hint: Format the total costs as MONEY for easier comparision.
 
--- 4a. How many CBSAs are in Tennessee? Warning: The cbsa table contains information for all states, not just Tennessee.
+SELECT 
+	CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
+		WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+		ELSE 'neither'
+		END AS drug_type,
+	SUM(total_drug_cost) AS total_cost
+FROM prescription
+LEFT JOIN drug
+USING(drug_name)
+GROUP BY drug_type
+ORDER BY total_cost DESC;
 
--- 4b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
+-- More money was spent on opioids (105,080,626.37) than antibiotic (38,435,121.26) - almost three times the cost of antiobiotics. 
 
--- 4c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.
 
--- 5a. Find all rows in the prescription table where total_claims is at least 3000. Report the drug_name and the total_claim_count.
+-- 5a. How many CBSAs are in Tennessee? Warning: The cbsa table contains information for all states, not just Tennessee.
 
--- 5b. For each instance that you found in part a, add a column that indicates whether the drug is an opioid.
+SELECT state, COUNT(cbsa)
+FROM fips_county
+LEFT JOIN cbsa
+USING(fipscounty)
+WHERE state = 'TN'
+GROUP BY state;
 
--- 5c. Add another column to you answer from the previous part which gives the prescriber first and last name associated with each row.
+-- 42 CSBA's in TN. 
 
--- The goal of this exercise is to generate a full list of all pain management specialists in Nashville and 
+-- 5b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
+
+
+(SELECT cbsaname, cbsa, SUM(population) as population, 'max' AS stats
+FROM cbsa
+INNER JOIN population 
+USING(fipscounty)
+GROUP BY 1, 2
+ORDER BY 3 DESC
+LIMIT 1)
+UNION
+(SELECT cbsaname, cbsa, SUM(population) as population, 'min' AS stats
+FROM cbsa
+INNER JOIN population 
+USING(fipscounty)
+GROUP BY 1, 2
+ORDER BY 3 
+LIMIT 1)
+
+-- Morristown, TN CSBA has the smallest combined population with 116,352.
+-- Nashville-Davidson--Murfreesboro--Franklin, TN CBSA has the largest with 1,830,410.
+
+-- 5c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.
+
+SELECT county, fipscounty, SUM(population) as population
+FROM population 
+LEFT JOIN cbsa
+USING(fipscounty)
+LEFT JOIN fips_county
+USING(fipscounty)
+WHERE cbsa IS NULL
+GROUP BY 1, 2
+ORDER BY 3 DESC
+LIMIT 1;
+
+-- Sevier County is the most populated county (with a population of 95,523) that does not have a CBSA. 
+
+
+-- 6a. Find all rows in the prescription table where total_claims is at least 3000. Report the drug_name and the total_claim_count.
+
+-- 6b. For each instance that you found in part a, add a column that indicates whether the drug is an opioid.
+
+-- 6c. Add another column to you answer from the previous part which gives the prescriber first and last name associated with each row.
+
+-- 7. The goal of this exercise is to generate a full list of all pain management specialists in Nashville and 
 -- the number of claims they had for each opioid. Hint: The results from all 3 parts will have 637 rows.
 
--- 6a. First, create a list of all npi/drug_name combinations for pain management specialists (specialty_description = 'Pain Management) 
+-- 7a. First, create a list of all npi/drug_name combinations for pain management specialists (specialty_description = 'Pain Management) 
 -- in the city of Nashville (nppes_provider_city = 'NASHVILLE'), where the drug is an opioid (opiod_drug_flag = 'Y'). 
 -- Warning: Double-check your query before running it. You will only need to use the prescriber and drug tables since you don't need the claims numbers yet.
 
--- 6b. Next, report the number of claims per drug per prescriber. Be sure to include all combinations, whether or not the prescriber had any claims. 
+-- 7b. Next, report the number of claims per drug per prescriber. Be sure to include all combinations, whether or not the prescriber had any claims. 
 -- You should report the npi, the drug name, and the number of claims (total_claim_count).
 
--- 6c. Finally, if you have not done so already, fill in any missing values for total_claim_count with 0. 
+-- 7c. Finally, if you have not done so already, fill in any missing values for total_claim_count with 0. 
 -- Hint - Google the COALESCE function.
